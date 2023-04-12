7540e3443b39f3451ef0eec653a266f02625ae96
SBiaa <developer.biaa@gmail.com>
2023-04-12 12:42:45 -0300

    finalizado

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,9 @@ let milliseconds = 0;
 let isPaused = false;
 
 startBtn.addEventListener("click", startTimer);
-
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer)
 
 function startTimer() {
 
@@ -32,9 +34,46 @@ function startTimer() {
       seconds = 0;
     }
 
-    minutesEl.textContent = minutes
-    secondsEl.textContent = seconds
-    millisecondsEl.textContent = milliseconds
-  }},10)
+    minutesEl.textContent = formatTime(minutes);
+    secondsEl.textContent = formatTime(seconds)
+    millisecondsEl.textContent = formatMilliseconds(milliseconds)
+  }},10);
+
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "block"
 }
 
+function pauseTimer() {
+  isPaused = true;
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "block"
+}
+
+function resumeTimer() {
+  isPaused = false;
+  pauseBtn.style.display = "block";
+  resumeBtn.style.display = "none"
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+
+  minutesEl.textContent = "00"
+  secondsEl.textContent = "00"
+  millisecondsEl.textContent = "000"
+
+  startBtn.style.display = "block";
+  resumeBtn.style.display = "none"
+  pauseBtn.style.display = "none"
+}
+
+function formatTime(time){
+  return time < 10 ? `0${time}` : time;
+}
+
+function formatMilliseconds(time) {
+  return time < 100 ? `${time}`.padStart(3, "0" ) : time;
+}
